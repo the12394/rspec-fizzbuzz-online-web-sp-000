2599d8bc089964cc7e56750fcd5f99cf143e452e
Thomas Eastman <the12394@gmail.com>
2021-05-29 18:39:31 +0000

    k

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,16 +4,10 @@
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-    if int % 3 == 0 && int % 5 == 0 
-    "FizzBuzz"
   end
   
   elsif int % 5 == 0 
     "Buzz"
-    if int % 3 == 0 && int % 5 == 0 
-    "FizzBuzz"
   end
- 
-else 
 
 end
